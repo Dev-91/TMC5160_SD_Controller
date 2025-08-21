702162525a333e35566fea2475a26c6debc3ac88
Dev91 <dev91@Dev91-MacBook-Pro.local>
2025-08-21 17:54:52 +0900

    main.h

---
+++ main/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "sdkconfig.h"
+
+#include "button_driver.h"
+#include "photo_interrupt.h"
+#include "tmc5160_SD_driver.h"
+
+#endif // MAIN_H
